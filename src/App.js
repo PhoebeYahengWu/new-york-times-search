2607bf5b01e45bf71453c52a58c65e1c01c14b7d
wuyaheng <wuyaheng2016@gmail.com>
2020-07-09 10:53:38 -0400

    change function component to class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Title from './components/Title/Title';
 import Search from './components/Search/Search';
 import Result from './components/Result/Result';
+import API from './utils/API';
 import './App.css';
 
-function App() {
-  return (
-    <>
-    <div className="container h-100">
-    <Title />   
-    <div className="row h-100 justify-content-center align-items-center">
-      <form className="col-10">
-      <Search />
-      <Result />
-      </form>
-    </div>
-    </div>
-    </>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <div className="container h-100">
+      <Title />   
+      <div className="row h-100 justify-content-center align-items-center">
+        <form className="col-10">
+        <Search />
+        <Result />
+        </form>
+      </div>
+      </div>
+    )
+  }
 }
 
 export default App;
