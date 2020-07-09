ef5bafd6ab6f9174a3fa26cd2811c74999aba34f
wuyaheng <wuyaheng2016@gmail.com>
2020-07-09 10:49:16 -0400

    add env file and api key

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,7 @@ export default {
     getUser: (query) =>
         axios({
             'method': 'GET',
-            'url': 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + query + '&api-key=' + apiKey,
+            'url': `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.REACT_APP_NYT_API_KEY}`,
             'headers': {
                 'content-type': 'application/json'
             }
